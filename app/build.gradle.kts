85be56610a94aa0e5c2c06948e4ff85535e824c3
snolls <alexperezsoto@gmail.com>
2024-11-12 11:36:02 +0100

    2º Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.1")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.7.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
 
     implementation(libs.appcompat)
     implementation(libs.material)
--- app/src/main/java/perezsoto/alejandro/pmdmtarea02/MainActivity.java
+++ app/src/main/java/perezsoto/alejandro/pmdmtarea02/MainActivity.java
@@ -8,17 +8,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import perezsoto.alejandro.pmdmtarea02.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
 
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment != null) {
+            navController = NavHostFragment.findNavController(navHostFragment);
+        }
+
         setContentView(binding.getRoot());
     }
 }
