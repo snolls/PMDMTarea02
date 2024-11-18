687f32df3c286c5b89ca2be9a23eaaa82286196b
snolls <alexperezsoto@gmail.com>
2024-11-18 16:08:51 +0100

    Commit

--- app/src/main/java/perezsoto/alejandro/pmdmtarea02/MainActivity.java
+++ app/src/main/java/perezsoto/alejandro/pmdmtarea02/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private NavHostFragment navHostFragment;
     private NavController navController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +50,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_about) { // El ID del ítem "Acerca de..."
+            showAboutDialog();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
+
+    // Método para mostrar el AlertDialog
+    private void showAboutDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Acerca de")
+                .setMessage("Aplicación desarrollada por Alejandro Perez Soto. Versión 1.0")
+                .setPositiveButton("Aceptar", (dialog, which) -> dialog.dismiss())
+                .show();
+    }
+
 }
